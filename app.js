61c9a0e02dfc147f16e8a31599dda32fd8ac8e8d
Nikolay Rusanov <nikolarusano92@gmail.com>
2024-03-20 20:09:16 +0200

    type Guard lesson

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 "use strict";
-const a = 1;
-console.log(a);
+let a = 5;
+let b = a.toString();
+let c = 'asdasd';
+let d = parseInt(c);
+let e = new String(a).valueOf();
+const user = {
+    name: 'Vasya',
+    email: 'vasiliy@yandex.ru',
+    login: 'vasia'
+};
+const admin = Object.assign(Object.assign({}, user), { role: 1 });
+console.log(admin);
--- app.ts
+++ app.ts
@@ -1,2 +1,38 @@
-const a = 1;
-console.log(a);
+interface User {
+	name: string;
+	email: string;
+	login: string;
+}
+
+const user: User = {
+	name: 'Vasya',
+	email: 'vasiliy@yandex.ru',
+	login: 'vasia'
+}
+
+interface Admin {
+	name: string,
+	role: number
+}
+
+
+function logId(id: string | number) {
+	if (typeof id === 'string') {
+		console.log(id);
+	} else if (typeof id === 'number') {
+		console.log(id);
+	}
+	id
+}
+
+function isString(x: string | number): x is string {
+	return typeof x === 'string';
+}
+
+function isAdmin(user: User | Admin): user is Admin {
+	return 'role' in user;
+}
+
+function setRole(user: User | Admin) {
+	if (user)
+}
